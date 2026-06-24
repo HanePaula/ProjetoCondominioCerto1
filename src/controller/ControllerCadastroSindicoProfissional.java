4a9b9d6a45c6ffa5565d537991fd6fb903ef1403
sinara-santinoni <sinara_santinoni@hotmail.com>
2026-06-23 21:04:15 -0300

    Adiciona ControllerCadastroSindicoProfissional

---
+++ src/controller/ControllerCadastroSindicoProfissional.java
@@ -0,0 +1,46 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import view.TelaCadastroSindicoProfissional;
+
+public class ControllerCadastroSindicoProfissional implements ActionListener {
+
+    TelaCadastroSindicoProfissional telaCadastroSindicoProfissional;
+
+    public ControllerCadastroSindicoProfissional(TelaCadastroSindicoProfissional telaCadastroSindicoProfissional) {
+        this.telaCadastroSindicoProfissional = telaCadastroSindicoProfissional;
+
+        this.telaCadastroSindicoProfissional.getjButtonNovo().addActionListener(this);
+        this.telaCadastroSindicoProfissional.getjButtonCancelar().addActionListener(this);
+        this.telaCadastroSindicoProfissional.getjButtonGravar().addActionListener(this);
+        this.telaCadastroSindicoProfissional.getjButtonBuscar().addActionListener(this);
+        this.telaCadastroSindicoProfissional.getjButtonSair().addActionListener(this);
+
+        utilities.Utilities.AtivaDesativa(this.telaCadastroSindicoProfissional.getjPanelbotoes(), true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent action) {
+        if (action.getSource() == this.telaCadastroSindicoProfissional.getjButtonNovo()) {
+
+            utilities.Utilities.AtivaDesativa(this.telaCadastroSindicoProfissional.getjPanelbotoes(), false);
+            utilities.Utilities.LimpaComponentes(this.telaCadastroSindicoProfissional.getjPanelDados(), true, this.telaCadastroSindicoProfissional.getButtonGroup1());
+
+        } else if (action.getSource() == this.telaCadastroSindicoProfissional.getjButtonCancelar()) {
+
+            utilities.Utilities.AtivaDesativa(this.telaCadastroSindicoProfissional.getjPanelbotoes(), true);
+            utilities.Utilities.LimpaComponentes(this.telaCadastroSindicoProfissional.getjPanelDados(), false, this.telaCadastroSindicoProfissional.getButtonGroup1());
+
+        } else if (action.getSource() == this.telaCadastroSindicoProfissional.getjButtonGravar()) {
+
+            utilities.Utilities.AtivaDesativa(this.telaCadastroSindicoProfissional.getjPanelbotoes(), true);
+            utilities.Utilities.LimpaComponentes(this.telaCadastroSindicoProfissional.getjPanelDados(), false, this.telaCadastroSindicoProfissional.getButtonGroup1());
+
+        } else if (action.getSource() == this.telaCadastroSindicoProfissional.getjButtonBuscar()) {
+
+        } else if (action.getSource() == this.telaCadastroSindicoProfissional.getjButtonSair()) {
+            this.telaCadastroSindicoProfissional.dispose();
+        }
+    }
+}
